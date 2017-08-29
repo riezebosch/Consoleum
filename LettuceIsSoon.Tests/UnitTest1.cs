2e2a7ff061d53b5f07856b83d1e7fbfcf744c599
Manuel Riezebosch <mriezebosch@gmail.com>
2017-08-29 11:46:16 +0200

    copy from clipboard working!

--- LettuceIsSoon.Tests/UnitTest1.cs
+++ LettuceIsSoon.Tests/UnitTest1.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Diagnostics;
 using System.Threading;
+using PInvoke;
 using WindowsInput;
 using WindowsInput.Native;
 using Xunit;
-using static PInvoke.Kernel32;
+using Shouldly;
+using System.Text;
 
 namespace LettuceIsSoon.Tests
 {
@@ -18,14 +20,29 @@ namespace LettuceIsSoon.Tests
             proc.StartInfo.UseShellExecute = true; // must always be false with .NET Core
             proc.Start();
             Thread.Sleep(1000);
-
+            
             var simulator = new InputSimulator();
             simulator.Keyboard.KeyDown(VirtualKeyCode.CONTROL);
             simulator.Keyboard.KeyPress(VirtualKeyCode.VK_A);
             simulator.Keyboard.KeyUp(VirtualKeyCode.CONTROL);
             simulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+            Thread.Sleep(200);
+
             simulator.Keyboard.KeyPress(VirtualKeyCode.VK_X);
             
+            User32.OpenClipboard(IntPtr.Zero).ShouldBeTrue();
+            unsafe 
+            {
+                var data = User32.GetClipboardData(13); // CF_UNICODETEXT: https://msdn.microsoft.com/nl-nl/library/windows/desktop/ff729168(v=vs.85).aspx
+                var text = Kernel32.GlobalLock(data);
+
+                var result = new string((char*)text);
+                Kernel32.GlobalUnlock(data);
+                User32.CloseClipboard().ShouldBeTrue();
+
+                result.ShouldBe("Hello World!\r\n");
+            }
+
             proc.WaitForExit();
         }
     }
